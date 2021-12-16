f4ad3feca29ed22e0f8f9b7b23b29e7ed5a520a7
racheld100 <62630113+racheld100@users.noreply.github.com>
2021-12-15 21:13:56 -0700

    Changed if/else statement layout

--- script.js
+++ script.js
@@ -3,36 +3,43 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword () {
-  var passwordLength = window.prompt("How long you you like your password to be? (8-128 characters)")
+  var characters = "";
+  var passwordLength = window.prompt("How long would you you like your password to be? (8-128 characters)")
   var integer = parseInt(passwordLength, 10);
-  if (integer >= "8" && integer <= "128") {
-    var prompt1 = window.prompt("Do you want uppercase letters? (yes or no)")  
-    var upperCase = prompt1.toLowerCase();
-    if (upperCase == "yes" || upperCase == "no") {
-      var prompt2 = window.prompt("Do you want numbers? (yes or no)")
-      var numeric = prompt2.toLowerCase();  
-        if (numeric == "yes" || numeric == "no") {
-          var prompt3 = window.prompt("Do you want special characters? (yes or no)")
-          var characters = prompt3.toLowerCase();  
-            if (characters == "yes" || characters == "no") {
-              passwordWriter ();
-            } else {
-              return;
-            }
-        } else {
-          return;
-        }
-    } else {
-      return;
-    }
-  } else {
+  if (integer < "8" || integer > "128") {
     return;
   }
+  var prompt1 = window.prompt("Do you want lowercase letters? (yes or no)")
+  var lowerCase = prompt1.toLowerCase();
+  if (lowerCase == "yes") {
+  characters = characters.concat("abcdefghijklmnopqrstuvwxyz");    
+  } else if (lowerCase != "no") {
+    return;
+  }
+  var prompt2 = window.prompt("Do you want uppercase letters? (yes or no)")  
+  var upperCase = prompt2.toLowerCase();
+  if (upperCase == "yes") {
+    var characters = characters.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  } else if (upperCase != "no") {
+    return;
+  }
+  var prompt3 = window.prompt("Do you want numbers? (yes or no)")
+  var numeric = prompt3.toLowerCase();  
+  if (numeric == "yes") {
+    var characters = characters.concat("0123456789"); 
+  } else if (numeric != "no") {
+    return;
+  }
+  var prompt4 = window.prompt("Do you want special characters? (yes or no)")
+  var specialCharacters = prompt4.toLowerCase();  
+  if (specialCharacters == "yes") {
+    var characters = characters.concat("!#$%&*+-/=<>?@[]\^_{}|~");
+  } else if (specialCharacters != "no") {
+    return;
+  }
+  // for (let )
 }
-
-function passwordWriter () {
-
-}
+  
 
 function writePassword() {
   var password = generatePassword();
